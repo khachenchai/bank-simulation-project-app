ba18d49f679f2e02e500b57b9a32bdafbc4e93c2
belll_ <ess.fuji5566@gmail.com>
2026-02-26 16:25:30 +0700

    user backend finish 1

--- backend/user.h
+++ backend/user.h
@@ -1,3 +1,28 @@
+#include <QVector>
+#include <QString>
 class User {
-
+        int id;
+        QString userid;
+        QString username;
+        QString password;
+        QString salt;
+        double balance;
+    public:
+        void loadDataFromFile();
+        void registerFunc(QString username,QString password, double balance);
+        void login(QString inputusername,QString inputpassword);
+        void reloadLoginUser();
+        void rewritetxt();
+        QVector<User> allUsers;
+        class Bank {
+            public:
+            bool topupFunc(QString selectedBank, double amount);
+        };
+        friend class Bank;
 };
+QString hashPassword(const QString& password, const QString& salt);
+QString generateSalt(int len = 16);
+QVector<QString> splitData(QString rowData, char separator);
+extern bool loginSuccess;
+extern User loginUser;
+extern int counter;
