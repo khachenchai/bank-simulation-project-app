9e631c1ef9ce936d0cf18ab5ccc659f1da31a8f8
khachenchai <jaiklaanda@gmail.com>
2026-03-02 15:12:36 +0700

    balance btn fix

--- topupdialog.cpp
+++ topupdialog.cpp
@@ -21,3 +21,55 @@ void TopUpDialog::on_ConfirmBtn_clicked()
     dlg.exec();
 }
 
+QString oriStyle = "padding: 4px;\nbackground: white;\nfont-size: 18px;\nborder-radius: 18px;\nborder: 2px solid #009E2A;\ncolor: #009E2A;\n";
+QString selectedStyle = "padding: 4px;\nbackground: #009E2A;\nfont-size: 18px;\nborder-radius: 18px;\nborder: 2px solid #009E2A;\ncolor: white;\n";
+
+void TopUpDialog::resetTopUpButtons()
+{
+
+    ui->Top100->setStyleSheet(oriStyle);
+    ui->Top300->setStyleSheet(oriStyle);
+    ui->Top500->setStyleSheet(oriStyle);
+    ui->Top1000->setStyleSheet(oriStyle);
+    ui->Top2000->setStyleSheet(oriStyle);
+}
+
+void TopUpDialog::on_Top100_clicked()
+{
+    resetTopUpButtons();
+    ui->amountEdit->setText("100");
+    ui->Top100->setStyleSheet(selectedStyle);
+}
+
+
+void TopUpDialog::on_Top300_clicked()
+{
+    resetTopUpButtons();
+    ui->amountEdit->setText("300");
+    ui->Top300->setStyleSheet(selectedStyle);
+}
+
+
+void TopUpDialog::on_Top500_clicked()
+{
+    resetTopUpButtons();
+    ui->amountEdit->setText("500");
+    ui->Top500->setStyleSheet(selectedStyle);
+}
+
+
+void TopUpDialog::on_Top1000_clicked()
+{
+    resetTopUpButtons();
+    ui->amountEdit->setText("1000");
+    ui->Top1000->setStyleSheet(selectedStyle);
+}
+
+
+void TopUpDialog::on_Top2000_clicked()
+{
+    resetTopUpButtons();
+    ui->amountEdit->setText("2000");
+    ui->Top2000->setStyleSheet(selectedStyle);
+}
+
